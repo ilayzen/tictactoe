9b181e0a799faeeee835e0626cb1a63244a6c130
Rostyslav Syz'ko <ilayzenoff@gmail.com>
2021-11-29 16:06:06 +0200

    test
    test

--- src/generateField.js
+++ src/generateField.js
@@ -1,26 +1,29 @@
 const ROWS_COUNT = 3;
 const COLS_COUNT = 3;
-const field = document.querySelector('.field');
+const field = document.querySelector(".field");
 
 function generateCols(row, colsCount, rowId) {
   for (let i = 0; i < colsCount; i++) {
     const id = rowId * ROWS_COUNT + i;
-    const col = document.createElement('div');
+    const col = document.createElement("div");
     col.id = `c-${id}`;
     col.dataset.id = id;
-    col.className = 'cell';
+    col.className = "cell";
     row.appendChild(col);
   }
 }
 
 function generateRows(rowsCount, colsCount) {
   for (let i = 0; i < rowsCount; i++) {
-    const row = document.createElement('div');
-    row.className = 'row';
+    const row = document.createElement("div");
+    row.className = "row";
     generateCols(row, colsCount, i);
     field.appendChild(row);
   }
 }
 
-generateRows(ROWS_COUNT, COLS_COUNT);
-// win horizontal
+export default function generateField() {
+  generateRows(ROWS_COUNT, COLS_COUNT);
+}
+
+export
